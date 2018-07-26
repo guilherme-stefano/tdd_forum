6efba98cff416e75eb3e284c63324e97337ab108
Guilherme Ow <guilherme.stefano@owinteractive.com>
2018-07-26 17:32:22 -0300

    added participateInForum test

---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ParticipateInForumTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $user = factory('App\User')->create();
+        $this->be($user); // auth
+
+        $thread = factory('App\Thread')->create();
+        
+        $reply = factory('App\Reply')->make();
+
+        $this->post('threads/'. $thread->id .'/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
