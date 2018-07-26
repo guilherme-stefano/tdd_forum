8c123b61379345122a44f875b127d96f9f95ebef
Guilherme Ow <guilherme.stefano@owinteractive.com>
2018-07-26 13:40:55 -0300

    migrations and initial tests

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadsTest extends TestCase
+{
+    // use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    /** @test */
+    public function a_user_cam_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
